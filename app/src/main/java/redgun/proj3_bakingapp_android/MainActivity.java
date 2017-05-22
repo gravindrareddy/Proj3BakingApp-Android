f6a7893b51b2e1cfcab853ecc001a69d03f26150
gravindrareddy <raviguntaka.reddy@gmail.com>
2017-05-23 01:52:52 +0530

    Initial commit

---
+++ app/src/main/java/redgun/proj3_bakingapp_android/MainActivity.java
@@ -0,0 +1,13 @@
+package redgun.proj3_bakingapp_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
