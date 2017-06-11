91b6087e54a08c47b70a67437d37258bca523038
gravindrareddy <raviguntaka.reddy@gmail.com>
2017-06-11 23:53:10 +0530

    + Created Sample widget

---
+++ app/src/main/java/redgun/bakingapp/RecipeWidgetProvider.java
@@ -0,0 +1,42 @@
+package redgun.bakingapp;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import java.util.Random;
+
+/**
+ * Created by Ravindra on 11-06-2017.
+ */
+
+
+public class RecipeWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int count = appWidgetIds.length;
+
+        for (int i = 0; i < count; i++) {
+            int widgetId = appWidgetIds[i];
+            String number = String.format("%03d", (new Random().nextInt(900) + 100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_recipe_ingredients);
+            // ToDo - Replace Recyler view as it is not supported by Widget Layout
+
+            remoteViews.setTextViewText(R.id.widget_recipe_ingridients_recyclerview, number);
+
+            Intent intent = new Intent(context, RecipeWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, widgetId);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.widget_recipe_ingredients_linearlayout, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
